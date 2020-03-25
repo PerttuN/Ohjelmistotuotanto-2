dfe3dbd8cbd50e9e440f0e55e87008f6f1cb7791
Perttuu <49315016+Perttuu@users.noreply.github.com>
2020-03-25 12:56:25 +0200

    Uusi versio demosta

--- server.js
+++ server.js
@@ -21,14 +21,42 @@ app.use(function (req, res, next) {
 });
 
 // Hae tuotteet
-app.get("/api/varastosaldo/haku", (req, res, next) => {
+app.get("/api/varasto_riistavesi/haku", (req, res, next) => {
     var nimi = req.query.nimi || '';
+    var sijainti = req.query.sijainti_ID || '';
 
-    var kysely = 'SELECT id, nimi, maara, yksikko, sijainti, kommentit FROM varastosaldo WHERE nimi LIKE ' + con.escape(nimi + '%');
+    var kysely = 'SELECT ID, nimi, maara, yksikkotyyppi_ID, yksikkohinta, saldo, sijainti_ID, kommentit FROM varasto_riistavesi WHERE nimi LIKE ' + con.escape('%' + nimi + '%') + ' AND sijainti_ID LIKE ' + con.escape(sijainti + '%');
+   // kysely + ' SELECT tuote.yksikkotyyppi_ID, yksikkotyyppi.nimi FROM tuote INNER JOIN yksikkotyyppi ON tuote.yksikkotyyppi_ID = yksikkotyyppi.ID';
 
     con.query(kysely, function (err, result, fields) {
         if (err) {
-            console.log('Virhe haettaessa dataa varastosaldo-taulusta, syy: ' + err);
+            console.log('Virhe haettaessa dataa tuote-taulusta, syy: ' + err);
+            res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
+        } else {
+            console.log(JSON.stringify(result));
+            res.status(200).json(result);
+        }
+    });
+});
+
+// Yksikkötyypin haku
+app.get("/api/varastokirjanpito/yksikkotyyppi", (req, res, next) => {
+    con.query('SELECT ID, nimi FROM yksikkotyyppi', function (err, result, fields) {
+        if (err) {
+            console.log('Virhe haettaessa dataa yksikkotyyppi-taulusta, syy: ' + err);
+            res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
+        } else {
+            console.log(JSON.stringify(result));
+            res.status(200).json(result);
+        }
+    });
+});
+
+// Sijainti haku
+app.get("/api/varastokirjanpito/sijainti", (req, res, next) => {
+    con.query('SELECT ID, nimi FROM sijainti', function (err, result, fields) {
+        if (err) {
+            console.log('Virhe haettaessa dataa sijainti-taulusta, syy: ' + err);
             res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
         } else {
             console.log(JSON.stringify(result));
@@ -38,12 +66,12 @@ app.get("/api/varastosaldo/haku", (req, res, next) => {
 });
 
 // Poista tuote
-app.get("/api/varastosaldo/poista", (req, res, next) => {
-    var id = req.query.id || '';
+app.get("/api/varasto_riistavesi/poista", (req, res, next) => {
+    var id = req.query.ID || '';
 
-    con.query('DELETE FROM varastosaldo WHERE id = ?', [id], function (err, result, fields) {
+    con.query('DELETE FROM varasto_riistavesi WHERE ID = ?', [id], function (err, result, fields) {
         if (err) {
-            console.log('Virhe poistettaessa dataa varastosaldo-taulusta, syy: ' + err);
+            console.log('Virhe poistettaessa dataa tuote-taulusta, syy: ' + err);
             res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
         } else {
             console.log(JSON.stringify(result));
@@ -53,26 +81,29 @@ app.get("/api/varastosaldo/poista", (req, res, next) => {
 });
 
 // Lisää tuote
-app.post("/api/varastosaldo/lisaa", (req, res, next) => {
+app.post("/api/varasto_riistavesi/lisaa", (req, res, next) => {
     if (typeof req.body.nimi === "undefined") {
         res.json({ "status": "not ok", "status_text": "nimi-kentt\u00e4 puuttuu" });
     }
     if (typeof req.body.maara === "undefined") {
         res.json({ "status": "not ok", "status_text": "m\u00e4\u00e4r\u00e4-kentt\u00e4 puuttuu" });
     }
-    if (typeof req.body.yksikko === "undefined") {
-        res.json({ "status": "not ok", "status_text": "yksikk\u00F6-kentt\u00e4 puuttuu" });
+    if (typeof req.body.yksikkotyyppi_ID === "undefined") {
+        res.json({ "status": "not ok", "status_text": "yksikk\u00F6tyyppi-kentt\u00e4 puuttuu" });
     }
-    if (typeof req.body.sijainti === "undefined") {
+    if (typeof req.body.yksikkohinta === "undefined") {
+        res.json({ "status": "not ok", "status_text": "yksikk\u00F6hinta-kentt\u00e4 puuttuu" });
+    }
+    if (typeof req.body.sijainti_ID === "undefined") {
         res.json({ "status": "not ok", "status_text": "sijainti-kentt\u00e4 puuttuu" });
     }
     if (typeof req.body.kommentit === "undefined") {
         res.json({ "status": "not ok", "status_text": "kommentit-kentt\u00e4 puuttuu" });
     }
 
-    con.query('INSERT INTO varastosaldo(nimi, maara, yksikko, sijainti, kommentit) VALUES (?, ?, ?, ?, ?)', [req.body.nimi, req.body.maara, req.body.yksikko, req.body.sijainti, req.body.kommentit], function (err, result, fields) {
+    con.query('INSERT INTO varasto_riistavesi(nimi, maara, yksikkotyyppi_ID, yksikkohinta, saldo, sijainti_ID, kommentit) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.body.nimi, req.body.maara, req.body.yksikkotyyppi_ID, req.body.yksikkohinta, req.body.saldo, req.body.sijainti_ID, req.body.kommentit], function (err, result, fields) {
         if (err) {
-            console.log('Virhe lis\u00e4tt\u00e4ess\u00e4 dataa varastosaldo-tauluun, syy: ' + err);
+            console.log('Virhe lis\u00e4tt\u00e4ess\u00e4 dataa tuote-tauluun, syy: ' + err);
             res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
         } else {
             console.log(JSON.stringify(result));
@@ -82,10 +113,10 @@ app.post("/api/varastosaldo/lisaa", (req, res, next) => {
 });
 
 // Hae yksittäisen tuotteen tiedot
-app.get("/api/varastosaldo/:id", (req, res, next) => {
-    con.query('SELECT id, nimi, maara, yksikko, sijainti, kommentit FROM varastosaldo WHERE id=' + con.escape(req.params.id), function (err, result, fields) {
+app.get("/api/varasto_riistavesi/:ID", (req, res, next) => {
+    con.query('SELECT ID, nimi, maara, yksikkotyyppi_ID, yksikkohinta, saldo, sijainti_ID, kommentit FROM varasto_riistavesi WHERE ID=' + con.escape(req.params.ID), function (err, result, fields) {
         if (err) {
-            console.log('Virhe haettaessa dataa varastosaldo-taulusta, syy: ' + err);
+            console.log('Virhe haettaessa dataa tuote-taulusta, syy: ' + err);
             res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
         } else {
             console.log(JSON.stringify(result));
@@ -95,26 +126,29 @@ app.get("/api/varastosaldo/:id", (req, res, next) => {
 });
 
 // Muokkaa yksittäisen tuotteen tietoja
-app.post("/api/varastosaldo/:id", (req, res, next) => {
+app.post("/api/varasto_riistavesi/:ID", (req, res, next) => {
     if (typeof req.body.nimi === "undefined") {
         res.json({ "status": "not ok", "status_text": "nimi-kentt\u00e4 puuttuu" });
     }
     if (typeof req.body.maara === "undefined") {
         res.json({ "status": "not ok", "status_text": "m\u00e4\u00e4r\u00e4-kentt\u00e4 puuttuu" });
     }
-    if (typeof req.body.yksikko === "undefined") {
-        res.json({ "status": "not ok", "status_text": "yksikk\u00F6-kentt\u00e4 puuttuu" });
+    if (typeof req.body.yksikkotyyppi_ID === "undefined") {
+        res.json({ "status": "not ok", "status_text": "yksikk\u00F6tyyppi-kentt\u00e4 puuttuu" });
     }
-    if (typeof req.body.sijainti === "undefined") {
+    if (typeof req.body.yksikkohinta === "undefined") {
+        res.json({ "status": "not ok", "status_text": "yksikk\u00F6hinta-kentt\u00e4 puuttuu" });
+    }
+    if (typeof req.body.sijainti_ID === "undefined") {
         res.json({ "status": "not ok", "status_text": "sijainti-kentt\u00e4 puuttuu" });
     }
     if (typeof req.body.kommentit === "undefined") {
         res.json({ "status": "not ok", "status_text": "kommentit-kentt\u00e4 puuttuu" });
     }
 
-    con.query('UPDATE varastosaldo SET nimi=?, maara=?, yksikko=?, sijainti=?, kommentit=? WHERE id=?', [req.body.nimi, req.body.maara, req.body.yksikko, req.body.sijainti, req.body.kommentit, req.params.id], function (err, result, fields) {
+    con.query('UPDATE varasto_riistavesi SET nimi=?, maara=?, yksikkotyyppi_ID=?, yksikkohinta=?, saldo=?, sijainti_ID=?, kommentit=? WHERE ID=?', [req.body.nimi, req.body.maara, req.body.yksikkotyyppi_ID, req.body.yksikkohinta, req.body.saldo, req.body.sijainti_ID, req.body.kommentit, req.params.ID], function (err, result, fields) {
         if (err) {
-            console.log('Virhe muokattaessa dataa varastosaldo-taulussa, syy: ' + err);
+            console.log('Virhe muokattaessa dataa tuote-taulussa, syy: ' + err);
             res.status(500).json({ 'status': 'not ok', 'status_text': err.sqlMessage });
         } else {
             console.log(JSON.stringify(result));
